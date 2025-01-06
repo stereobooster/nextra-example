7ac56e0ea18c7155df0b388f8fb61108eaf1f0d0
stereobooster <stereobooster@gmail.com>
2025-01-06 21:41:17 +0100

    fix

--- pages/notes/_meta.ts
+++ pages/notes/_meta.ts
@@ -1,10 +1,10 @@
-// import { createCatchAllMeta } from "nextra/catch-all";
+import { createCatchAllMeta } from "nextra/catch-all";
 
 // Note: for remote MDX `_meta.js` should return a function instead of an object
-// export default () => createCatchAllMeta(["index"]);
+export default () => createCatchAllMeta(["index.mdx", "contact.mdx", "about.mdx"]);
 
-export default () => ({
-  index: 'My Homepage',
-  contact: 'Contact Us',
-  about: 'About Us'
-});
+// export default () => ({
+//   index: 'My Homepage',
+//   contact: 'Contact Us',
+//   about: 'About Us'
+// });
